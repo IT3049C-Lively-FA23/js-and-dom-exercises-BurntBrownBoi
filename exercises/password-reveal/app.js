f4d737632de150c15cb76e264abf825d60bab35a
Moatiz Sabahat <moatiz.sabahat@hotmail.com>
2023-10-22 21:44:47 -0400

    Password reveal + Cypress setup

--- exercises/password-reveal/app.js
+++ exercises/password-reveal/app.js
@@ -1,7 +1,19 @@
 // 🤖: create 2 constants with references to the password input and the reveal button from the DOM
-
+const passwordInput = document.getElementById('password');
+const toggleButton = document.getElementById('togglePassword');
 
 // 🤖: Create an Event Listener on the password reveal button
+toggleButton.addEventListener('click', function() {
+  if(passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    toggleButton.classList.remove('bi-eye-slash');
+    toggleButton.classList.add('bi-eye');
+  } else {
+    passwordInput.type = 'password';
+    toggleButton.classList.remove('bi-eye');
+    toggleButton.classList.add('bi-eye-slash');
+  }
+});
 // 🤖: The event listener should toggle the password field attribute between `password` and `text` to change the visibility of the password field content.
 
 
